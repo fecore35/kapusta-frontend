6275e393918d32d4e12b524783af96aa9cecf410
axer4 <79149727+axer4@users.noreply.github.com>
2022-02-05 14:30:00 +0200

    Допилил до минимально-работающего)
    допилил для мвп, Игорь просил сильно

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,13 +1,15 @@
 import styles from './HomePage.module.scss';
 import kapusta from './kapusta2.png';
+import SignupForm from './SignUpForm/SignUpForm';
 function HomePage() {
   return (
-    <div>
+    <div className={styles.homepage}>
       <div className={styles.hero}>
         <div className={styles.tagline}>
           <h1 className={styles.heading}>Kapu$ta</h1>
           <p className={styles.description}>Smart finance</p>
         </div>
+        <SignupForm className={styles.signup} />
       </div>
       <img src={kapusta} alt="kapusta" className={styles.kapusta} />
     </div>
