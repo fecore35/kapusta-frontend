d27e8bbb8a23d6994b1c1d479ad511bc0adfb2c6
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-07 19:08:52 +0200

    Delete duplicate Compononts

--- src/Components/ExampleButton/exampleButton.js
+++
@@ -1,7 +0,0 @@
-import s from './exampleButton.module.scss'
-
-function ExampleButton() {
-
-    return <button className={s.buttonMain + ' ' + s.buttonText + ' ' + s.exampleClass}> This is Style Example Button</button >
-}
-export default ExampleButton
