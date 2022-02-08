45001272cfb82b6c0c0614bb56d7a8a273a2052f
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-09 00:02:01 +0200

    add state for Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Router from '../constants/router';
 
 function Dashboard() {
+  const [tab, setTab] = useState('income');
+
   return (
     <>
       <h2>Dashboard</h2>
       <Link to={Router.REPORT}>Перейти к отчетам</Link>
+
+      <div>
+        <button type="button" onClick={() => setTab('income')}>
+          доход
+        </button>
+        <button type="button" onClick={() => setTab('consumption')}>
+          расход
+        </button>
+      </div>
+
+      {tab === 'income' ? <>доход</> : <>расход</>}
     </>
   );
 }
