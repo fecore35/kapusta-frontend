a3b098c85cd95a3f45dfb96725a1a41f3b63df74
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-08 11:21:58 +0200

    fix: modal style

--- src/Components/ModalConfirm/ModalConfirm.js
+++ src/Components/ModalConfirm/ModalConfirm.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import s from './ModalConfirm.module.scss';
 
-import { success, info, defaultModules, defaults } from '@pnotify/core';
-import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
-import * as PNotifyCountdown from '@pnotify/countdown';
+import { success, defaults } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import * as Confirm from '@pnotify/confirm';
 import '@pnotify/confirm/dist/PNotifyConfirm.css';
-import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
 
 defaults.closerHover = false;
-defaultModules.set(PNotifyBootstrap4, {});
 defaults.addModelessClass = 'logOutModal';
-defaults.styling = s;
-// defaults.hide = false;
+defaults.width = '380px';
+defaults.styling = 'custom';
+defaults.hide = false;
 
 const ModalConfirm = () => {
   function click() {
