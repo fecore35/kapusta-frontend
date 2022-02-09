f480d6ce7694d62af155e9a1f6c9694eb6119ecc
axer4 <79149727+axer4@users.noreply.github.com>
2022-02-09 21:57:43 +0200

    вроде как есть
    Если не брать в учёт border-radius и адаптивку.. то он полностью готов, осталось дождаться данных с БД, подвязал с кнопками расход/доход , а так же с кнопками переключения категорий, там в консоль вывел как будет отрабатывать когда будет что выводить, так же подумал о том , что с БД цифры могут приходить как угодно, по этому перед отрисовкой сортирую массив по возрастанию что бы не было конфузов

---
+++ src/components/homepage/schedule/Shedule.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react';
+import * as V from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryLabel } from 'victory';
+import styles from './Schedule.module.scss'
+function Schedule({type,category}) {
+    const [data,setData] = useState( [
+        { name: "Свинина", sum: 3000, },
+        { name: "Говядина", sum: 500, },
+        { name: "Курица", sum: 1800, },
+        { name: "Рыба", sum: 2100, },
+        { name: "Панини", sum: 1800, },
+        { name: "Кофе", sum: 1700, },
+        { name: "Спагетти", sum: 1500, },
+        { name: "Шоколад", sum: 800, },
+        { name: "Маслины", sum: 500, },
+        { name: "Зелень", sum: 300, },
+                 
+    ])
+    const [tick, setTick] = useState(data);
+    console.log(category)
+    const sort = () => tick.sort((a, b) => {
+        return b.sum - a.sum
+    })
+    const dataForSchedule = () => tick.sort((a, b) => {
+        return b.sum - a.sum
+    }).map(el => {
+                            return {
+                                quarter: el.name,
+                                earnings: el.sum,
+                                label: el.sum,
+                                fill:'#FF751D'
+                            }
+    })
+    // console.log(dataForSchedule())
+    
+    useEffect(() => {
+if (type !== "Расход") {
+    return setTick([
+    
+        {
+            name: "Жены",
+            sum:20000,
+        },
+        {
+        name: "Моя",
+        sum:25000
+    },
+    ])
+        }
+        setTick(data)
+    }, [type])
+    useEffect(() => {
+    console.log("Изменили категорию - получаем новые данные с БД по выбранной категории и перерисовываем график")
+    },[category])
+        
+    
+    return <div className={styles.container}>
+        <div className={styles.schedule}>
+        <VictoryChart
+            theme={VictoryTheme.material}
+                width={620}
+                padding={60}
+            >
+                <VictoryAxis
+                    // tickFormat={}
+                />
+            <VictoryBar
+                style={{
+                     data: {
+                        width: 36,
+                        fill: ({ datum }) => datum.fill,
+                        },
+                }}
+                // data={[
+                //     { quarter: "Свинина", earnings: 5000, label: "5000", fill: '#FF751D',},
+                //     { quarter: "Говядина", earnings: 4500, label: "4500", fill:'#FFDAC0' },
+                //     { quarter: "Курица", earnings: 3200, label: "3200" ,fill:'#FFDAC0'},
+                //     { quarter: "Рыба", earnings: 2100, label: "2100", fill: '#FF751D' },
+                //     { quarter: "Панини", earnings: 1800, label: "1800" ,fill:'#FFDAC0'},
+                //     { quarter: "Кофе", earnings: 1700, label: "1700" ,fill:'#FFDAC0'},
+                //     { quarter: "Спагетти", earnings: 1500, label: "1500", fill: '#FF751D' },
+                //     { quarter: "Шоколад", earnings: 800, label: "800" ,fill:'#FFDAC0'},
+                //     { quarter: "Маслины", earnings: 550, label: "550",fill:'#FFDAC0' },
+                //     { quarter: "Зелень", earnings: 300, label: "300", fill: '#FF751D' },
+                // ]}
+                    data={dataForSchedule()}
+        // data accessor for x values
+        x="quarter"
+        // data accessor for y values
+                y="earnings"
+                labelComponent = { <VictoryLabel
+                style={[
+                    {    fontFamily: 'Roboto',
+                        fontSize:12,
+                        lineHeight: 14,
+                    }
+                ]}
+            />}
+            />
+            </VictoryChart>
+    </div>
+        </div>
+}
+export default Schedule
