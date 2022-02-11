93670b70ff17b45a0885f9b75f55008e4050bcba
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-11 23:59:30 +0200

    fix: enpoint

--- src/redux/report/reportOperation.js
+++ src/redux/report/reportOperation.js
@@ -5,7 +5,9 @@ const getCategory = createAsyncThunk(
   'report/category',
   async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.get('/transactions/stats', credentials);
+      const { data } = await axios.get(
+        `/transactions/stats/${credentials.month}/${credentials.year}`,
+      );
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
