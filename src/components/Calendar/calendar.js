cd1cdd4517975677193e9dd32e23742f6dfc860c
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-14 10:16:12 +0200

    add extraData to redux

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -3,11 +3,19 @@ import Calendar from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import ru from 'date-fns/locale/ru';
 
+import { useDispatch } from 'react-redux';
+import { initDate } from 'redux/extraData/extraDataReducer';
+
 function DatePicker() {
   const [value, onChange] = useState(new Date(), 'yyyy-MM-dd');
+  const dispatch = useDispatch();
 
   const handleDateChange = date => {
     onChange(date);
+    const year = String(date.getFullYear());
+    const month = String(date.getMonth() + 1);
+    const day = String(date.getDate());
+    dispatch(initDate({ month, day, year }));
   };
 
   return (
---
+++ src/redux/extraData/index.js
@@ -0,0 +1,1 @@
+export { default as extraDataSelectors } from './extraDataSelectors';
