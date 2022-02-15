6d00c7ab29f7c36d33da57442c7256e3f54de842
Sta11k <Alexandiel1984@gmail.com>
2022-02-15 15:15:10 +0200

    fix header

--- src/components/AppBar/buttonUserName/ButtonUserName.js
+++ src/components/AppBar/buttonUserName/ButtonUserName.js
@@ -1,17 +1,21 @@
 import s from '../AppBar.module.scss';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Router from '../../../constants/router';
 
 const ButtonUserName = () => {
   const name = useSelector(state => state.auth.name);
   return (
-    <button
-      className={s.buttonUserName}
-      variant="outlined"
-      type="button"
-      // onClick={() => dispatch(authOperations.UserName())}
-    >
-      {name}
-    </button>
+    <Link to={Router.DASHBOARD}>
+      <button
+        className={s.buttonUserName}
+        variant="outlined"
+        type="button"
+        // onClick={() => dispatch(authOperations.UserName())}
+      >
+        {name}
+      </button>
+    </Link>
   );
 };
 
