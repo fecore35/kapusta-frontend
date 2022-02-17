78fac1d3ce1520c2289a046db9f2c168d944d014
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-17 14:44:10 +0200

    bugfix: user name

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,43 +2,31 @@
 import { useSelector } from 'react-redux';
 
 import logo from '../../icons/logoKapusta.svg';
-// import groupV from '../../icons/userActiv.svg';
 import vector1 from '../../icons/logoKapusta.svg';
 import ButtonUserName from './buttonUserName/ButtonUserName';
 import ButtonLogOut from './buttonLogOut/ButtonLogOut';
 import s from './AppBar.module.scss';
 
+import { Link } from 'react-router-dom';
+import Router from 'constants/router';
+
 const AppBar = () => {
-  //   const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
   const isAuth = useSelector(state => state.auth.isAuth);
   const name = useSelector(state => state.auth.name);
-  // const reg = /\w+\s/g;
-  // let newName = name.match(reg);
-  let symbolName = name[0].toUpperCase();
-  console.log(symbolName);
+  let symbolName = name && name[0].toUpperCase();
+
   return (
     <div className={s.container}>
       <header className={s.header}>
-        <img
-          // className={s.header__image}
-          src={logo}
-          alt="Logo"
-          width="90"
-          height="31"
-          className={s.logo}
-        />
+        <img src={logo} alt="Logo" width="90" height="31" className={s.logo} />
 
         {isAuth && (
           <div className={s.user__item}>
             <div className={s.user__symbol}>
-              <p>{symbolName}</p>
+              <Link to={Router.DASHBOARD}>
+                <p>{symbolName}</p>
+              </Link>
             </div>
-            {/* <img src={groupV} alt="group42" width="32" className={s.groupV} /> */}
-
-            {/* <img src={vector1} alt="vector1" width="32" className={s.vector1} /> */}
-
-            {/* <Navigation /> */}
-            {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
             <ButtonUserName className={s.buttonUserName} />
             <img
               src={vector1}
