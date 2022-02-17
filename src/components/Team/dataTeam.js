e7628fa23b7d0a85ac44c1e2e790dfd23d19c2a9
fecore35 <79923560+fecore35@users.noreply.github.com>
2022-02-17 15:39:57 +0200

    add team info

--- src/components/Team/dataTeam.js
+++ src/components/Team/dataTeam.js
@@ -1,12 +1,12 @@
-import Dennys from 'images/team/Dennys.jpg'
-import Rostyslav from 'images/team/Rostyslav.jpg'
-import Aleksey from 'images/team/Aleksey.jpg'
-import Evgenii from 'images/team/Evgenii.jpg'
-import Ihor from 'images/team/Ihor.jpg'
-import Vlad from 'images/team/Vlad.jpg'
-import Valentin from 'images/team/Valentin.jpg'
-import Oleksandr from 'images/team/Oleksandr.jpg'
-import Alex from 'images/team/Alex.jpg'
+import Dennys from 'images/team/Dennys.jpg';
+import Rostyslav from 'images/team/Rostyslav.jpg';
+import Aleksey from 'images/team/Aleksey.jpg';
+import Evgenii from 'images/team/Evgenii.jpg';
+import Ihor from 'images/team/Ihor.jpg';
+import Vlad from 'images/team/Vlad.jpg';
+import Valentin from 'images/team/Valentin.jpg';
+import Oleksandr from 'images/team/Oleksandr.jpg';
+import Alex from 'images/team/Alex.jpg';
 
 const team = [
   {
@@ -38,7 +38,8 @@ const team = [
     fullName: 'Oleksandr Dushkin',
     position: 'Front-End',
     photo: Oleksandr,
-    linkedIn: '#',
+    linkedIn:
+      'https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-%D0%B4%D1%83%D1%88%D0%BA%D0%B8%D0%BD-964246b1/',
     gitHub: 'https://github.com/AleksDu',
   },
   {
@@ -47,14 +48,14 @@ const team = [
     position: 'Front-End',
     photo: Alex,
     linkedIn: '#',
-    gitHub: 'https://github.com/',
+    gitHub: 'https://github.com/Sta11k',
   },
   {
     id: 6,
     fullName: 'Valentin Rybalov',
     position: 'Front-End',
     photo: Valentin,
-    linkedIn: '#',
+    linkedIn: 'www.linkedin.com/in/valentyn-rybalov',
     gitHub: 'https://github.com/RybalovValentyn',
   },
   {
@@ -63,23 +64,23 @@ const team = [
     position: 'Front-End',
     photo: Aleksey,
     linkedIn: '#',
-    gitHub: 'https://github.com/',
+    gitHub: 'https://github.com/axer4',
   },
   {
     id: 8,
     fullName: 'Ihor Yatsyshyn',
     position: 'Back-End',
     photo: Ihor,
-    linkedIn: '#',
-    gitHub: 'https://github.com/',
+    linkedIn: 'https://www.linkedin.com/in/ihor-yatsyshyn/',
+    gitHub: 'https://github.com/Yatsa-dev',
   },
   {
     id: 9,
     fullName: 'Rostyslav Perig',
     position: 'Back-End',
     photo: Rostyslav,
-    linkedIn: '#',
-    gitHub: 'https://github.com/',
+    linkedIn: 'https://www.linkedin.com/in/rostyslav-perig-7a81a2222/',
+    gitHub: 'https://github.com/Rost-Perig',
   },
-]
-export default team
+];
+export default team;
